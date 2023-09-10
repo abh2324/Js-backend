c73facff799864f8079c66c36c6c74a8aea430c6
abh2324 <102346208+abh2324@users.noreply.github.com>
2023-09-10 15:20:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'./public')));
 
 
-app.get('/',(req,res) => { 
+app.get('/home',(req,res) => { 
     res.sendFile(path.join(__dirname,'./public/index.html'));
 });
 
